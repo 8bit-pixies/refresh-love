0970b2ac7d906e22a2296f29bb65889775345d67
chapman siu <chapm0n.siu@gmail.com>
2014-07-18 23:08:59 +1000

    update book

--- _book/functions/helper.lua
+++ _book/functions/helper.lua
@@ -1,22 +1,14 @@
-function love.load()
-
+function loadMap()
   Tileset = love.graphics.newImage('countryside.png')
 
   TileW, TileH = 32,32
-  local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
 
-    local quadInfo = {
-      {'.', 0, 0},  -- grass
-      {'#', 32, 0}, -- box
-      {'*', 0, 32}, -- flowers
-      {'^',32, 32}  -- boxtop
-    }
-
-    Quads = {}
-    for _, info in ipairs(quadInfo) do
-      -- info[1] = char, info[2] = x, info[3] = y
-      Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW, TileH, tilesetW, tilesetH)
-    end
+  local quadInfo = {
+    {'.', 0, 0},  -- grass
+    {'#', 32, 0}, -- box
+    {'*', 0, 32}, -- flowers
+    {'^',32, 32}  -- boxtop
+  }
 
   local tileString = [[
 ...
@@ -24,10 +16,17 @@ function love.load()
 ...
 ]]
 
+  local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
+  Quads = {}
+  for _, info in ipairs(quadInfo) do
+    -- info[1] = char, info[2] = x, info[3] = y
+    Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW, TileH, tilesetW, tilesetH)
+  end
+  
   TileTable = {}
   local width = #(tileString:match("[^\n]+"))
   for x=1,width,1 do TileTable[x] = {} end -- initilaise correct dim
-  
+
   local rowIndex, columnIndex = 1,1
   for row in tileString:gmatch("[^\n]+") do
     columnIndex = 1
@@ -36,10 +35,10 @@ function love.load()
       columnIndex = columnIndex + 1
     end
     rowIndex = rowIndex+1
-  end  
+  end
 end
 
-function love.draw()
+function drawMap()
   for rowIndex, row in ipairs(TileTable) do
     for colIndex, chr in ipairs(row) do
       local x,y = 250+(colIndex-1)*TileW, 250+(rowIndex-1)*TileH
@@ -47,4 +46,3 @@ function love.draw()
     end
   end
 end
-
